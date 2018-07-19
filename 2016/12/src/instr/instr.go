428b523afb50ccd147d542aafadfe9b223cd2267
Matthew Simmons <simmonmt@acm.org>
2018-07-19 17:20:33 -0600

    2016 day 12

---
+++ 2016/12/src/instr/instr.go
@@ -0,0 +1,190 @@
+package instr
+
+import (
+	"fmt"
+	"reg"
+	"strconv"
+	"strings"
+)
+
+type Instr interface {
+	Exec(file *reg.File) int
+	String() string
+}
+
+type regBase struct {
+	r1, r2 *reg.Reg
+	imm    uint32
+	off    int
+}
+
+type cpy struct {
+	regBase
+}
+
+func newCpyReg(r1, r2 reg.Reg) Instr {
+	return &cpy{regBase{r1: &r1, r2: &r2}}
+}
+
+func newCpyImm(imm uint32, r2 reg.Reg) Instr {
+	return &cpy{regBase{imm: imm, r2: &r2}}
+}
+
+func (i *cpy) Exec(file *reg.File) int {
+	var imm uint32 = i.imm
+	if i.r1 != nil {
+		imm = file.Get(*i.r1)
+	}
+
+	file.Set(*i.r2, imm)
+	return 1
+}
+
+func (i *cpy) String() string {
+	if i.r1 == nil {
+		return fmt.Sprintf("cpy %d, %s", i.imm, *i.r2)
+	} else {
+		return fmt.Sprintf("cpy %v, %s", i.r1, *i.r2)
+	}
+}
+
+type inc struct {
+	regBase
+}
+
+func newInc(r reg.Reg) Instr {
+	return &inc{regBase{r1: &r}}
+}
+
+func (i *inc) Exec(file *reg.File) int {
+	file.Set(*i.r1, file.Get(*i.r1)+1)
+	return 1
+}
+
+func (i *inc) String() string {
+	return fmt.Sprintf("inc %s", *i.r1)
+}
+
+type dec struct {
+	regBase
+}
+
+func newDec(r reg.Reg) Instr {
+	return &dec{regBase{r1: &r}}
+}
+
+func (i *dec) Exec(file *reg.File) int {
+	file.Set(*i.r1, file.Get(*i.r1)-1)
+	return 1
+}
+
+func (i *dec) String() string {
+	return fmt.Sprintf("dec %s", *i.r1)
+}
+
+type jnz struct {
+	regBase
+}
+
+func newJnzReg(r1 reg.Reg, off int) Instr {
+	return &jnz{regBase{r1: &r1, off: off}}
+}
+
+func newJnzImm(imm uint32, off int) Instr {
+	return &jnz{regBase{imm: imm, off: off}}
+}
+
+func (i *jnz) Exec(file *reg.File) int {
+	var val uint32 = i.imm
+	if i.r1 != nil {
+		val = file.Get(*i.r1)
+	}
+
+	if val != 0 {
+		return i.off
+	}
+	return 1
+}
+
+func (i *jnz) String() string {
+	if i.r1 == nil {
+		return fmt.Sprintf("jnz %d, %d", i.imm, i.off)
+	} else {
+		return fmt.Sprintf("jnz %s, %d", *i.r1, i.off)
+	}
+}
+
+func parseImm(str string) (uint32, error) {
+	str = strings.TrimLeft(str, "+")
+	val, err := strconv.ParseUint(str, 10, 32)
+	return uint32(val), err
+}
+
+func parseOffset(str string) (int, error) {
+	str = strings.TrimLeft(str, "+")
+	val, err := strconv.ParseInt(str, 10, 32)
+	return int(val), err
+}
+
+func Parse(op, a, b string) (Instr, error) {
+	reg1, reg1Err := reg.FromString(a)
+	reg2, reg2Err := reg.FromString(b)
+
+	switch op {
+	case "cpy":
+		if reg2Err != nil {
+			return nil, fmt.Errorf("%v reg2 parse fail: %v", op, reg2Err)
+		}
+
+		if reg1Err != nil {
+			imm, immErr := parseImm(a)
+			if immErr != nil {
+				return nil, fmt.Errorf("%v reg1/imm parse fail: %v", op, immErr)
+			}
+			return newCpyImm(imm, reg2), nil
+		} else {
+			return newCpyReg(reg1, reg2), nil
+		}
+		break
+
+	case "inc":
+		fallthrough
+	case "dec":
+		if reg1Err != nil {
+			return nil, fmt.Errorf("%v reg1 parse fail: %v", op, reg1Err)
+		}
+		if b != "" {
+			return nil, fmt.Errorf("%v takes one arg: %v r1", op, op)
+		}
+
+		switch op {
+		case "inc":
+			return newInc(reg1), nil
+		case "dec":
+			return newDec(reg1), nil
+		default:
+			panic("unreachable")
+		}
+
+	case "jnz":
+		off, offErr := parseOffset(b)
+		if offErr != nil {
+			return nil, fmt.Errorf("jmp offset parse fail: %v", offErr)
+		}
+
+		if reg1Err != nil {
+			imm, immErr := parseImm(a)
+			if immErr != nil {
+				return nil, fmt.Errorf("%v reg1/imm parse fail: %v", op, immErr)
+			}
+			return newJnzImm(imm, off), nil
+		} else {
+			return newJnzReg(reg1, off), nil
+		}
+
+	default:
+		return nil, fmt.Errorf("unknown op %v", op)
+	}
+
+	panic("unreachable")
+}
---
+++ 2016/12/src/instr/instr_test.go
@@ -0,0 +1,122 @@
+package instr
+
+import (
+	"strconv"
+	"testing"
+
+	"reg"
+)
+
+func verifyFile(t *testing.T, f *reg.File, expA, expB uint32) {
+	if f.Get(reg.A) != expA || f.Get(reg.B) != expB {
+		t.Errorf("reg file unexpected, got A=%v b=%v, want A=%v b=%v",
+			f.Get(reg.A), f.Get(reg.B), expA, expB)
+	}
+}
+
+func testSingleRegInstr(t *testing.T, i Instr, initB, expB uint32) {
+	f := reg.NewFile()
+	f.Set(reg.A, 99)
+	f.Set(reg.B, initB)
+
+	if rPC := i.Exec(f); rPC != 1 {
+		t.Errorf("\"%v\" rPC = %v, want 1", rPC)
+	}
+	verifyFile(t, f, 99, expB)
+}
+
+func TestInc(t *testing.T) {
+	i := newInc(reg.B)
+	if s := i.String(); s != "inc b" {
+		t.Errorf("i = \"%s\", want \"inc b\"", s)
+	}
+
+	testSingleRegInstr(t, i, 8, 9)
+}
+
+func TestDec(t *testing.T) {
+	i := newDec(reg.B)
+	if s := i.String(); s != "dec b" {
+		t.Errorf("i = \"%s\", want \"inc b\"", s)
+	}
+
+	testSingleRegInstr(t, i, 8, 7)
+}
+
+func TestJnzReg(t *testing.T) {
+	if s := newJnzReg(reg.A, 12).String(); s != "jnz a, 12" {
+		t.Errorf("i = \"%v\", want \"jnz a, 12\"", s)
+	}
+
+	f := reg.NewFile()
+	f.Set(reg.A, 5)
+	f.Set(reg.B, 0)
+
+	if rPC := newJnzReg(reg.A, 12).Exec(f); rPC != 12 {
+		t.Errorf("jnz a, 12 rPC = %v, want 12", rPC)
+	}
+	if rPC := newJnzReg(reg.B, 12).Exec(f); rPC != 1 {
+		t.Errorf("jnz b, 12 rPC = %v, want 1", rPC)
+	}
+	verifyFile(t, f, 5, 0)
+}
+
+func TestJnzImm(t *testing.T) {
+	if s := newJnzImm(1, 12).String(); s != "jnz 1, 12" {
+		t.Errorf("i = \"%v\", want \"jnz 1, 12\"", s)
+	}
+
+	f := reg.NewFile()
+	f.Set(reg.A, 5)
+	f.Set(reg.B, 0)
+
+	if rPC := newJnzImm(5, 12).Exec(f); rPC != 12 {
+		t.Errorf("jnz 5, 12 rPC = %v, want 12", rPC)
+	}
+	if rPC := newJnzImm(0, 12).Exec(f); rPC != 1 {
+		t.Errorf("jnz 0, 12 rPC = %v, want 1", rPC)
+	}
+	verifyFile(t, f, 5, 0)
+}
+
+func TestParse(t *testing.T) {
+	type testCase struct {
+		op, a, b string
+		output   string
+	}
+
+	testCases := []testCase{
+		testCase{"cpy", "a", "b", "cpy a, b"},
+		testCase{"cpy", "a", "", ""},
+		testCase{"cpy", "a", "invalid", ""},
+		testCase{"cpy", "invalid", "b", ""},
+
+		testCase{"cpy", "12", "a", "cpy 12, a"},
+		testCase{"cpy", "-12", "a", ""},
+		testCase{"cpy", "a", "12", ""},
+
+		testCase{"inc", "a", "", "inc a"},
+		testCase{"dec", "a", "", "dec a"},
+
+		testCase{"jnz", "a", "+1", "jnz a, 1"},
+		testCase{"jnz", "a", "1", "jnz a, 1"},
+		testCase{"jnz", "a", "-1", "jnz a, -1"},
+		testCase{"jnz", "a", "b", ""},
+
+		testCase{"jnz", "1", "1", "jnz 1, 1"},
+		testCase{"jnz", "1", "a", ""},
+	}
+
+	for i, tc := range testCases {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			inst, err := Parse(tc.op, tc.a, tc.b)
+			if err != nil && tc.output != "" {
+				t.Errorf("%v %v %v: expected nil error, got %v", tc.op, tc.a, tc.b, err)
+			} else if err == nil && tc.output == "" {
+				t.Errorf("%v %v %v: expected non-nil error, got nil", tc.op, tc.a, tc.b)
+			} else if err == nil && inst.String() != tc.output {
+				t.Errorf("%v %v %v: expected '%v' got '%s'", tc.op, tc.a, tc.b, tc.output, inst)
+			}
+		})
+	}
+}
