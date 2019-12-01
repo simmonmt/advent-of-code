b5895ca4da9605a379a99606334959e4ef470a94
Matthew Simmons <simmonmt@acm.org>
2019-12-01 10:00:10 -0500

    use flag to specify input file

--- tpl/src/main.go
+++ tpl/src/main.go
@@ -12,12 +12,19 @@ import (
 
 var (
 	verbose = flag.Bool("verbose", false, "verbose")
+	input = flag.String("input", "", "input file")
 )
 
-func readInput() ([]string, error) {
+func readInput(path string) ([]string, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
 	lines := []string{}
 
-	scanner := bufio.NewScanner(os.Stdin)
+	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
 		line := scanner.Text()
 		lines = append(lines, line)
@@ -33,7 +40,11 @@ func main() {
 	flag.Parse()
 	logger.Init(*verbose)
 
-	lines, err := readInput()
+	if *input == "" {
+		log.Fatalf("--input is required")
+	}
+
+	lines, err := readInput(*input)
 	if err != nil {
 		log.Fatal(err)
 	}
