970cd687fb0f6b9a1ec1e25297503fc38b527b52
Matthew Simmons <simmonmt@acm.org>
2019-12-07 12:48:42 -0500

    make code generic so we can use it for 05b too

--- 2019/05/a/src/main.go
+++ 2019/05/a/src/main.go
@@ -15,31 +15,28 @@ import (
 
 var (
 	verbose = flag.Bool("verbose", false, "verbose")
-	input   = flag.String("input", "", "input file")
+	ramPath = flag.String("ram", "", "path to file containing ram values")
+	input   = flag.String("input", "", "input values")
 )
 
-func readInput(path string) ([]string, error) {
+func readRam(path string) (vm.Ram, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	lines := []string{}
+	var line string
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
+		line = scanner.Text()
+		break
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("read failed: %v", err)
 	}
 
-	return lines, nil
-}
-
-func initRam(line string) (vm.Ram, error) {
 	ram := vm.NewRam()
 	for i, str := range strings.Split(line, ",") {
 		val, err := strconv.Atoi(str)
@@ -52,25 +49,42 @@ func initRam(line string) (vm.Ram, error) {
 	return ram, nil
 }
 
+func parseInput(inputStr string) ([]int, error) {
+	out := []int{}
+	for _, s := range strings.Split(inputStr, ",") {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse input value %v: %v", s, err)
+		}
+
+		out = append(out, v)
+	}
+	return out, nil
+}
+
 func main() {
 	flag.Parse()
 	logger.Init(*verbose)
+
+	if *ramPath == "" {
+		log.Fatalf("--ram is required")
+	}
 
 	if *input == "" {
 		log.Fatalf("--input is required")
 	}
 
-	lines, err := readInput(*input)
+	ram, err := readRam(*ramPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	ram, err := initRam(lines[0])
+	inputValues, err := parseInput(*input)
 	if err != nil {
-		log.Fatalf("ram init fail: %v", err)
+		log.Fatal(err)
 	}
 
-	io := vm.NewIO(1)
+	io := vm.NewIO(inputValues...)
 
 	if err := vm.Run(ram, io, 0); err != nil {
 		log.Fatalf("program failed: %v", err)
