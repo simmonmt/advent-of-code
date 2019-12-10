3e94bbe34d7a65288d1dece8badec61c657b3414
Matthew Simmons <simmonmt@acm.org>
2019-12-10 09:53:32 -0500

    add factoring routine for 2019 10a

---
+++ 2019/10/a/src/puzzle/factor.go
@@ -0,0 +1,29 @@
+package puzzle
+
+var (
+	kPrimes = []int{
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+	}
+)
+
+func Factor(n, d int) (int, int) {
+	for {
+		changed := false
+		for _, p := range kPrimes {
+			if p > n && p > d {
+				break
+			}
+
+			if n%p == 0 && d%p == 0 {
+				n /= p
+				d /= p
+				changed = true
+			}
+		}
+		if !changed {
+			break
+		}
+	}
+
+	return n, d
+}
---
+++ 2019/10/a/src/puzzle/factor_test.go
@@ -0,0 +1,27 @@
+package puzzle
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestFactor(t *testing.T) {
+	type TestCase struct {
+		n, d   int
+		en, ed int
+	}
+
+	testCases := []TestCase{
+		TestCase{4, 6, 2, 3},
+		TestCase{12, 18, 2, 3},
+	}
+
+	for _, tc := range testCases {
+		t.Run(fmt.Sprintf("%d/%d", tc.n, tc.d), func(t *testing.T) {
+			if n, d := Factor(tc.n, tc.d); n != tc.en || d != tc.ed {
+				t.Errorf("Factor(%d,%d) = %d,%d, want %d,%d",
+					tc.n, tc.d, n, d, tc.en, tc.ed)
+			}
+		})
+	}
+}
