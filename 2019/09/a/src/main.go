3d62a594717aee8abd783638f7f78b332990b1a9
Matthew Simmons <simmonmt@acm.org>
2019-12-09 21:08:32 -0500

    add working main that runs programs

--- 2019/09/a/src/main.go
+++ 2019/09/a/src/main.go
@@ -6,48 +6,89 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 
+	vm "github.com/simmonmt/aoc/2019/09/a/src/vm"
 	"github.com/simmonmt/aoc/2019/common/logger"
 )
 
 var (
 	verbose = flag.Bool("verbose", false, "verbose")
-	input = flag.String("input", "", "input file")
+	ramPath = flag.String("ram", "", "path to file containing ram values")
+	input   = flag.String("input", "", "input values")
 )
 
-func readInput(path string) ([]string, error) {
+func readRam(path string) (vm.Ram, error) {
 	f, err := os.Open(path)
 	if err != nil {
 		return nil, err
 	}
 	defer f.Close()
 
-	lines := []string{}
+	var line string
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
+		line = scanner.Text()
+		break
 	}
 	if err := scanner.Err(); err != nil {
 		return nil, fmt.Errorf("read failed: %v", err)
 	}
 
-	return lines, nil
+	ram := vm.NewRam()
+	for i, str := range strings.Split(line, ",") {
+		val, err := strconv.Atoi(str)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse %v: %v", str, err)
+		}
+		ram.Write(i, val)
+	}
+
+	return ram, nil
+}
+
+func parseInput(inputStr string) ([]int, error) {
+	out := []int{}
+	for _, s := range strings.Split(inputStr, ",") {
+		v, err := strconv.Atoi(s)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse input value %v: %v", s, err)
+		}
+
+		out = append(out, v)
+	}
+	return out, nil
 }
 
 func main() {
 	flag.Parse()
 	logger.Init(*verbose)
 
+	if *ramPath == "" {
+		log.Fatalf("--ram is required")
+	}
+
 	if *input == "" {
 		log.Fatalf("--input is required")
 	}
 
-	lines, err := readInput(*input)
+	ram, err := readRam(*ramPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(lines)
+	inputValues, err := parseInput(*input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	io := vm.NewSaverIO(inputValues...)
+
+	if err := vm.Run(ram, io, 0); err != nil {
+		log.Fatalf("program failed: %v", err)
+	}
+
+	fmt.Printf("output: %v\n", io.Written())
 }
