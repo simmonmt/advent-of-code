f9b84345cc06a9bba36cb8746a7e22a5504147a8
Matthew Simmons <simmonmt@acm.org>
2019-12-09 20:05:22 -0500

    add a resources object

--- 2019/09/a/src/vm/vm.go
+++ 2019/09/a/src/vm/vm.go
@@ -33,80 +33,102 @@ func readBytes(ram Ram, pc, sz int) []int {
 	return out
 }
 
-func Run(ram Ram, io IO, pc int) error {
+type Resources struct {
+	ram Ram
+	io  IO
+}
+
+func decode(r *Resources, pc int) (Instruction, error) {
+	var inst Instruction
+
+	val := r.ram.Read(pc)
+
+	op := val % 100
+
+	ctorA := makeCtor((val / 100) % 10)
+	ctorB := makeCtor((val / 1000) % 10)
+	ctorC := makeCtor((val / 10000) % 10)
+
+	switch op {
+	case 1:
+		inst = &Add{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+			c: ctorC(r.ram.Read(pc + 3)),
+		}
+		break
+	case 2:
+		inst = &Multiply{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+			c: ctorC(r.ram.Read(pc + 3)),
+		}
+		break
+	case 3:
+		inst = &Input{a: ctorA(r.ram.Read(pc + 1))}
+		break
+	case 4:
+		inst = &Output{a: ctorA(r.ram.Read(pc + 1))}
+		break
+	case 5:
+		inst = &JumpIfTrue{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+		}
+		break
+	case 6:
+		inst = &JumpIfFalse{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+		}
+		break
+	case 7:
+		inst = &LessThan{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+			c: ctorC(r.ram.Read(pc + 3)),
+		}
+		break
+	case 8:
+		inst = &Equals{
+			a: ctorA(r.ram.Read(pc + 1)),
+			b: ctorB(r.ram.Read(pc + 2)),
+			c: ctorC(r.ram.Read(pc + 3)),
+		}
+		break
+	case 99:
+		inst = &Halt{}
+		break
+
+	default:
+		return nil, fmt.Errorf("bad opcode %d at %d", op, pc)
+	}
+
+	logger.LogF("%d: %s %v", pc, inst.String(), readBytes(r.ram, pc, inst.Size()))
+	return inst, nil
+}
+
+func run(r *Resources, pc int) error {
 	for {
-		var inst Instruction
-
-		val := ram.Read(pc)
-
-		op := val % 100
-
-		ctorA := makeCtor((val / 100) % 10)
-		ctorB := makeCtor((val / 1000) % 10)
-		ctorC := makeCtor((val / 10000) % 10)
-
-		switch op {
-		case 1:
-			inst = &Add{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-				c: ctorC(ram.Read(pc + 3)),
-			}
-			break
-		case 2:
-			inst = &Multiply{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-				c: ctorC(ram.Read(pc + 3)),
-			}
-			break
-		case 3:
-			inst = &Input{a: ctorA(ram.Read(pc + 1))}
-			break
-		case 4:
-			inst = &Output{a: ctorA(ram.Read(pc + 1))}
-			break
-		case 5:
-			inst = &JumpIfTrue{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-			}
-			break
-		case 6:
-			inst = &JumpIfFalse{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-			}
-			break
-		case 7:
-			inst = &LessThan{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-				c: ctorC(ram.Read(pc + 3)),
-			}
-			break
-		case 8:
-			inst = &Equals{
-				a: ctorA(ram.Read(pc + 1)),
-				b: ctorB(ram.Read(pc + 2)),
-				c: ctorC(ram.Read(pc + 3)),
-			}
-			break
-		case 99:
-			inst = &Halt{}
-			break
-
-		default:
-			return fmt.Errorf("bad opcode %d at %d", op, pc)
+		inst, err := decode(r, pc)
+		if err != nil {
+			return err
 		}
 
-		logger.LogF("%d: %s %v", pc, inst.String(), readBytes(ram, pc, inst.Size()))
-
-		npc := inst.Execute(ram, io, pc)
+		npc := inst.Execute(r.ram, r.io, pc)
 		if npc == -1 {
 			return nil
 		}
 
 		pc = npc
 	}
+}
+
+func Run(ram Ram, io IO, pc int) error {
+	r := &Resources{
+		ram: ram,
+		io:  io,
+	}
+
+	return run(r, pc)
 }
