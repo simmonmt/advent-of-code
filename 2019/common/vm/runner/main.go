8a401e4daaeee7e180ffb3be2ea2bdb302471698
Matthew Simmons <simmonmt@acm.org>
2019-12-14 14:29:19 -0500

    use new common routines

--- 2019/common/vm/runner/main.go
+++ 2019/common/vm/runner/main.go
@@ -1,15 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
-	"os"
-	"strconv"
-	"strings"
 
 	"github.com/simmonmt/aoc/2019/common/logger"
+	"github.com/simmonmt/aoc/2019/common/strutil"
 	"github.com/simmonmt/aoc/2019/common/vm"
 )
 
@@ -18,49 +15,6 @@ var (
 	ramPath = flag.String("ram", "", "path to file containing ram values")
 	input   = flag.String("input", "", "input values")
 )
-
-func readRam(path string) (vm.Ram, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	var line string
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line = scanner.Text()
-		break
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("read failed: %v", err)
-	}
-
-	ram := vm.NewRam()
-	for i, str := range strings.Split(line, ",") {
-		val, err := strconv.Atoi(str)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse %v: %v", str, err)
-		}
-		ram.Write(int64(i), int64(val))
-	}
-
-	return ram, nil
-}
-
-func parseInput(inputStr string) ([]int64, error) {
-	out := []int64{}
-	for _, s := range strings.Split(inputStr, ",") {
-		v, err := strconv.Atoi(s)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse input value %v: %v", s, err)
-		}
-
-		out = append(out, int64(v))
-	}
-	return out, nil
-}
 
 func main() {
 	flag.Parse()
@@ -74,12 +28,12 @@ func main() {
 		log.Fatalf("--input is required")
 	}
 
-	ram, err := readRam(*ramPath)
+	ram, err := vm.NewRamFromFile(*ramPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	inputValues, err := parseInput(*input)
+	inputValues, err := strutil.StringToInt64s(*input)
 	if err != nil {
 		log.Fatal(err)
 	}
