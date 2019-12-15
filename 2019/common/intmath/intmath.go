562998d9688df1ad299e9a540edd0480ea261f4a
Matthew Simmons <simmonmt@acm.org>
2019-12-15 10:09:30 -0500

    add naive gcd to common

--- 2019/common/intmath/intmath.go
+++ 2019/common/intmath/intmath.go
@@ -42,3 +42,41 @@ func AtoiOrDie(s string) int {
 	}
 	return val
 }
+
+var (
+	kPrimes = []int{
+		2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97,
+	}
+)
+
+func GCD(vs ...int) int {
+	d := 1
+
+	for _, p := range kPrimes {
+		for {
+			all := true
+			for _, v := range vs {
+				if v < p || v%p != 0 {
+					all = false
+				}
+			}
+
+			if !all {
+				break
+			}
+
+			d *= p
+			for i := range vs {
+				vs[i] /= p
+			}
+		}
+	}
+
+	for _, v := range vs {
+		if v > kPrimes[len(kPrimes)-1] {
+			panic(fmt.Sprintf("%d too big", v))
+		}
+	}
+
+	return d
+}
