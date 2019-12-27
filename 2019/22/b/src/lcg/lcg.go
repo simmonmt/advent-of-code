931c2b0ec4c2d39ff6d655b0ea5f4deb18ade7f0
Matthew Simmons <simmonmt@acm.org>
2019-12-26 22:23:07 -0500

    non-scaling ffwd that works

--- 2019/22/b/src/lcg/lcg.go
+++ 2019/22/b/src/lcg/lcg.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"math/big"
 )
 
@@ -34,31 +35,104 @@ var (
 // 99998: 49742805710047
 // 99999: 87378128651512
 
-func computeANSlow(a, mod *big.Int, n int64) *big.Int {
-	val := big.NewInt(0)
-	val.Exp(a, big.NewInt(n), nil)
-	return val
+func extGCD(a, b int) (s, t int) {
+	x, y, u, v := 0, 1, 1, 0
+	for a != 0 {
+		q, r := b/a, b%a
+		m, n := x-u*q, y-v*q
+		b, a, x, y, u, v = a, r, u, v, m, n
+	}
+
+	return x, y
 }
 
-func computeANFast(a, mod *big.Int, n int64) *big.Int {
-	// fast .. but doesn't work yet
+func computeANSlow(a, mod big.Int, n int64) big.Int {
+	val := big.NewInt(0)
+	val.Exp(&a, big.NewInt(n), &mod)
+	return *val
+}
+
+// computes a^n*x mod m
+func computeANX(a, x *big.Int, n int64, m *big.Int) *big.Int {
+	// from
 	// https://en.wikipedia.org/w/index.php?title=Modular_exponentiation&action=edit&section=3
 
-	base := big.NewInt(0)
-	*base = *a
+	base := &big.Int{}
+	base.Set(a)
+	base.Mod(base, m)
 
-	result := big.NewInt(1)
-	base.Mod(base, mod)
+	result := &big.Int{}
+	result.Set(x)
 	for n > 0 {
 		if (n & 1) != 0 {
 			result.Mul(result, base)
-			result.Mod(result, mod)
+			result.Mod(result, m)
 		}
 		n >>= 1
 		base.Mul(base, base)
-		base.Mod(base, mod)
+		base.Mod(base, m)
 	}
 	return result
+}
+
+func computeForward(x *big.Int, n int64, a, c, m *big.Int) *big.Int {
+	// fast forwarding and reversing described here
+	// https://www.nayuki.io/page/fast-skipping-in-a-linear-congruential-generator
+
+	// We're computing
+	//
+	//                    (a^n-1) mod (a-1)m
+	//  ( (a^n*x mod m) + ------------------ b ) mod m
+	//                            a-1
+
+	//	fmt.Printf("x %v n %v a %v c %v m %v\n", x, n, a, c, m)
+
+	aMinus1 := &big.Int{}
+	aMinus1.Sub(a, big.NewInt(1))
+
+	num := &big.Int{}
+	num.Exp(a, big.NewInt(n), nil)
+	num.Sub(num, big.NewInt(1))
+
+	frac := &big.Int{}
+	frac.Div(num, aMinus1)
+	frac.Mul(frac, c)
+
+	//val := computeANX(a, x, n, m)
+	val := &big.Int{}
+	val.Exp(a, big.NewInt(n), nil)
+	val.Mul(val, x)
+	val.Add(val, frac)
+	val.Mod(val, m)
+
+	// //fmt.Printf("a-1 %v\n", aMinus1)
+
+	// // num[erator]M = (a-1)m
+	// numM := &big.Int{}
+	// numM.Set(aMinus1)
+	// numM.Mul(numM, m)
+
+	// // (a^n-1) mod (a-1)m
+	// //
+	// // Which we compute as ((a^n mod (a-1)m) - 1) mod (a-1)m
+	// num := computeANX(a, big.NewInt(1), n, numM)
+	// num.Sub(num, big.NewInt(1))
+	// num.Mod(num, numM)
+
+	// num = computeANX(a, big.NewInt(1), n, m)
+	// num.Sub(num, big.NewInt(1))
+	// num.Mul(num, c)
+
+	// frac := &big.Int{}
+	// frac.Div(num, aMinus1)
+
+	// // a^n*x mod m + the fraction
+	// val := computeANX(a, x, n, m)
+	// val.Add(val, frac)
+	// val.Mod(val, m)
+
+	//fmt.Printf("x %v n %v a %v c %v m %v\n", x, n, a, c, m)
+	return val
 }
 
 func main() {
@@ -76,43 +150,49 @@ func main() {
 
 	seed := big.NewInt(*seed)
 
-	val := big.NewInt(0)
-	*val = *seed
+	for i := int64(2); i < 10; i++ {
+		slow := computeANSlow(*a, *mod, i)
+		fast := computeANX(a, big.NewInt(1), i, mod)
+		if slow.Cmp(fast) != 0 {
+			log.Fatal("slow vs fast verify failed at %d: %v vs %v",
+				i, slow, fast)
+		}
+	}
+	fmt.Println("computeANFast verify succeeded")
+
+	forwardMatch := true
+	val := &big.Int{}
+	val.Set(seed)
 	for i := 0; i < 10; i++ {
-		fmt.Printf("%d: %v\n", i, val)
+		//fmt.Println("---")
+
+		ffwd := computeForward(seed, int64(i), a, c, mod)
+
+		fmt.Printf("%d: %-20v", i, val)
+		if ffwd.Cmp(val) == 0 {
+			fmt.Println("match")
+		} else {
+			fmt.Printf("mismatch %v\n", ffwd)
+			forwardMatch = false
+		}
 
 		val.Mul(val, a)
 		val.Add(val, c)
 		val.Mod(val, mod)
 	}
+	if !forwardMatch {
+		log.Fatalf("forward test failed")
+	}
 
-	x := big.NewInt(0)
-	*x = *seed
+	s, t := extGCD(int(a.Int64()), int(mod.Int64()))
 
-	n := int64(8)
+	fmt.Printf("s %v t %v\n", s, t)
 
-	// fast forwarding and reversing described here
-	// https://www.nayuki.io/page/fast-skipping-in-a-linear-congruential-generator
+	inv := &big.Int{}
+	inv.Mul(a, big.NewInt(int64(t)))
+	inv.Mod(a, mod)
 
-	an := computeANSlow(a, mod, n)
-
-	num, numMod, den, frac := big.NewInt(0), big.NewInt(0), big.NewInt(0), big.NewInt(0)
-
-	numMod.Sub(a, big.NewInt(1))
-	numMod.Mul(numMod, mod)
-
-	num.Sub(an, big.NewInt(1))
-	num.Mod(num, numMod)
-
-	den.Sub(a, big.NewInt(1))
-	frac.Div(num, den)
-	frac.Mul(frac, c)
-
-	result := big.NewInt(0)
-	result.Mul(an, seed)
-	result.Mod(result, mod)
-	result.Add(result, frac)
-	result.Mod(result, mod)
+	fmt.Printf("as = %v\n", inv)
 
 	// Tried fast forwarding with
 	// n=101741582076661, which gave 1608694956433: wrong
@@ -121,6 +201,4 @@ func main() {
 	// many repetitions.
 	//
 	// we want to find the y that gives us the result 2020
-
-	fmt.Printf("%v\n", result)
 }
