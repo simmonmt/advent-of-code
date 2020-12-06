99666a1daf14e2f40b3d14a50f21630f583f5276
Matthew Simmons <simmonmt@acm.org>
2020-12-06 13:46:58 -0500

    Use the new filereader interfaces for new days

--- 2020/tpl/src/main.go
+++ 2020/tpl/src/main.go
@@ -1,40 +1,18 @@
 package main
 
 import (
-	"bufio"
 	"flag"
 	"fmt"
 	"log"
-	"os"
 
+	"github.com/simmonmt/aoc/2020/common/filereader"
 	"github.com/simmonmt/aoc/2020/common/logger"
 )
 
 var (
 	verbose = flag.Bool("verbose", false, "verbose")
-	input = flag.String("input", "", "input file")
+	input   = flag.String("input", "", "input file")
 )
-
-func readInput(path string) ([]string, error) {
-	f, err := os.Open(path)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-
-	lines := []string{}
-
-	scanner := bufio.NewScanner(f)
-	for scanner.Scan() {
-		line := scanner.Text()
-		lines = append(lines, line)
-	}
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("read failed: %v", err)
-	}
-
-	return lines, nil
-}
 
 func main() {
 	flag.Parse()
@@ -44,7 +22,7 @@ func main() {
 		log.Fatalf("--input is required")
 	}
 
-	lines, err := readInput(*input)
+	lines, err := filereader.Lines(*input)
 	if err != nil {
 		log.Fatal(err)
 	}
