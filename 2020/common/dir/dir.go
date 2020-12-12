576d81101222993ca10e900e7e2a9ae428fab099
Matthew Simmons <simmonmt@acm.org>
2020-12-12 16:19:26 -0500

    Add dir Left, Right, Parse, dist for From

--- 2020/common/dir/dir.go
+++ 2020/common/dir/dir.go
@@ -16,6 +16,21 @@ var (
 	AllDirs = []Dir{DIR_NORTH, DIR_SOUTH, DIR_WEST, DIR_EAST}
 )
 
+func Parse(str string) Dir {
+	switch str {
+	case "N":
+		return DIR_NORTH
+	case "S":
+		return DIR_SOUTH
+	case "W":
+		return DIR_WEST
+	case "E":
+		return DIR_EAST
+	default:
+		panic("bad dir")
+	}
+}
+
 func (d Dir) String() string {
 	switch d {
 	case DIR_NORTH:
@@ -46,16 +61,50 @@ func (d Dir) Reverse() Dir {
 	}
 }
 
-func (d Dir) From(p pos.P2) pos.P2 {
+func (d Dir) Left() Dir {
 	switch d {
 	case DIR_NORTH:
-		return pos.P2{X: p.X, Y: p.Y - 1}
+		return DIR_WEST
 	case DIR_SOUTH:
-		return pos.P2{X: p.X, Y: p.Y + 1}
-	case DIR_EAST:
-		return pos.P2{X: p.X + 1, Y: p.Y}
+		return DIR_EAST
 	case DIR_WEST:
-		return pos.P2{X: p.X - 1, Y: p.Y}
+		return DIR_SOUTH
+	case DIR_EAST:
+		return DIR_NORTH
+	default:
+		panic("bad dir")
+	}
+}
+
+func (d Dir) Right() Dir {
+	switch d {
+	case DIR_NORTH:
+		return DIR_EAST
+	case DIR_SOUTH:
+		return DIR_WEST
+	case DIR_WEST:
+		return DIR_NORTH
+	case DIR_EAST:
+		return DIR_SOUTH
+	default:
+		panic("bad dir")
+	}
+}
+
+func (d Dir) From(p pos.P2) pos.P2 {
+	return d.StepsFrom(p, 1)
+}
+
+func (d Dir) StepsFrom(p pos.P2, num int) pos.P2 {
+	switch d {
+	case DIR_NORTH:
+		return pos.P2{X: p.X, Y: p.Y - 1*num}
+	case DIR_SOUTH:
+		return pos.P2{X: p.X, Y: p.Y + 1*num}
+	case DIR_EAST:
+		return pos.P2{X: p.X + 1*num, Y: p.Y}
+	case DIR_WEST:
+		return pos.P2{X: p.X - 1*num, Y: p.Y}
 	default:
 		panic("bad dir")
 	}
