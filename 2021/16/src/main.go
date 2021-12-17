0bcbb0329032423c9798758c9980142e64d44a4d
Matthew Simmons <simmonmt@acm.org>
2021-12-17 11:54:58 -0500

    2021 day 16a

---
+++ 2021/16/src/main.go
@@ -0,0 +1,298 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"strconv"
+
+	"github.com/simmonmt/aoc/2021/common/filereader"
+	"github.com/simmonmt/aoc/2021/common/logger"
+)
+
+var (
+	verbose = flag.Bool("verbose", false, "verbose")
+	input   = flag.String("input", "", "input file")
+)
+
+func readInput(path string) (string, error) {
+	lines, err := filereader.Lines(*input)
+	if err != nil {
+		return "", err
+	}
+
+	if len(lines) != 1 {
+		return "", fmt.Errorf("too many lines: %v, want 1", len(lines))
+	}
+
+	return lines[0], err
+}
+
+type ArrView struct {
+	base []byte
+	off  int
+}
+
+func NewArrView(a []byte) *ArrView {
+	return &ArrView{
+		base: a,
+		off:  0,
+	}
+}
+
+func (v *ArrView) Consume(l int) ([]byte, error) {
+	if l > len(v.base)-v.off {
+		return nil, fmt.Errorf("short read")
+	}
+
+	out := v.base[v.off : v.off+l]
+	v.off += l
+	return out, nil
+}
+
+func (v *ArrView) Left() int {
+	return len(v.base) - v.off
+}
+
+func (v *ArrView) Off() int {
+	return v.off
+}
+
+func (v *ArrView) Rest() []byte {
+	return v.base[v.off:]
+}
+
+type Packet struct {
+	Version int
+	ID      int
+	Literal int
+	Subs    []*Packet
+}
+
+func (p *Packet) DumpTo(w io.Writer) {
+	p.dump(w, "")
+}
+
+func (p *Packet) dump(w io.Writer, indent string) {
+	fmt.Fprintf(w, "%vVer %v ID %v Lit %v\n", indent, p.Version, p.ID, p.Literal)
+	if len(p.Subs) > 0 {
+		fmt.Fprintf(w, "%v{\n", indent)
+
+		for i, sub := range p.Subs {
+			if i > 0 {
+				fmt.Fprintf(w, "%v}, {\n", indent)
+			}
+
+			sub.dump(w, indent+"  ")
+		}
+
+		fmt.Fprintf(w, "%v}\n", indent)
+	}
+}
+
+func hexToBin(hex string) (string, error) {
+	out := ""
+	for _, r := range hex {
+		d, err := strconv.ParseInt(string(r), 16, 8)
+		if err != nil {
+			return "", err
+		}
+		s := strconv.FormatInt(d, 2)
+		for len(s) < 4 {
+			s = "0" + s
+		}
+
+		out += s
+	}
+	return out, nil
+}
+
+func consumeStr(v *ArrView, l int) (string, error) {
+	out, err := v.Consume(l)
+	if err != nil {
+		return "", err
+	}
+
+	return string(out), err
+}
+
+func consumeInt(v *ArrView, l int) (int, error) {
+	s, err := consumeStr(v, l)
+	if err != nil {
+		return 0, err
+	}
+
+	n, err := strconv.ParseUint(s, 2, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return int(n), nil
+}
+
+func decode(in string) (*Packet, error) {
+	in, err := hexToBin(in)
+	if err != nil {
+		return nil, fmt.Errorf("parse error: %v", err)
+	}
+
+	v := NewArrView([]byte(in))
+	return decodePacket(v)
+}
+
+func decodePacket(rest *ArrView) (*Packet, error) {
+	logger.LogF("decodePacket %v: %v", rest.Off(), string(rest.Rest()))
+
+	wrapErr := func(where string, sub error) error {
+		return fmt.Errorf("%d: %v: %v", rest.Off(), where, sub)
+	}
+
+	version, err := consumeInt(rest, 3)
+	if err != nil {
+		return nil, wrapErr("version", err)
+	}
+
+	id, err := consumeInt(rest, 3)
+	if err != nil {
+		return nil, wrapErr("id", err)
+	}
+
+	p := &Packet{
+		Version: version,
+		ID:      id,
+	}
+
+	logger.LogF("packet header %+v rest %v: %v",
+		p, rest.Off(), string(rest.Rest()))
+
+	if id == 4 {
+		lit, err := decodeLiteral(rest)
+		if err != nil {
+			return nil, wrapErr("literal", err)
+		}
+
+		logger.LogF("decoded literal %v", lit)
+
+		p.Literal = lit
+		return p, nil
+	}
+
+	lenTypeID, err := consumeInt(rest, 1)
+	if err != nil {
+		return nil, wrapErr("len type id", err)
+	}
+
+	if lenTypeID == 0 {
+		var bitLen int
+		bitLen, err = consumeInt(rest, 15)
+		if err != nil {
+			return nil, wrapErr("mode 0 bitlen", err)
+		}
+
+		subs := []*Packet{}
+		for bitLen > 0 {
+			left := rest.Left()
+			sub, err := decodePacket(rest)
+			if err != nil {
+				return nil, wrapErr("mode 0 sub packet", err)
+			}
+
+			consumed := left - rest.Left()
+			subs = append(subs, sub)
+
+			bitLen -= consumed
+			if bitLen < 0 {
+				panic("underflow")
+			}
+		}
+
+		p.Subs = subs
+	} else {
+		var numPackets int
+		numPackets, err = consumeInt(rest, 11)
+		if err != nil {
+			return nil, wrapErr("mode 1 num packets", err)
+		}
+
+		subs := []*Packet{}
+		for i := 0; i < numPackets; i++ {
+			var sub *Packet
+			sub, err = decodePacket(rest)
+			if err != nil {
+				return nil, wrapErr("mode 1 sub packet", err)
+			}
+
+			subs = append(subs, sub)
+		}
+
+		p.Subs = subs
+	}
+
+	return p, nil
+}
+
+func decodeLiteral(rest *ArrView) (int, error) {
+	out := 0
+	for {
+		var n int
+		var err error
+		n, err = consumeInt(rest, 5)
+		if err != nil {
+			return 0, err
+		}
+
+		out = (out << 4) | (n & 0xf)
+		if n&0x10 == 0 {
+			break
+		}
+	}
+	return out, nil
+}
+
+func versionSum(packet *Packet) int {
+	sum := packet.Version
+	for _, sub := range packet.Subs {
+		sum += versionSum(sub)
+	}
+	return sum
+}
+
+func solveA(line string) {
+	p, err := decode(line)
+	if err != nil {
+		log.Fatalf("bad decode: %v", err)
+	}
+
+	fmt.Println("A", versionSum(p))
+}
+
+func main() {
+	flag.Parse()
+	logger.Init(*verbose)
+
+	if *input == "" {
+		log.Fatalf("--input is required")
+	}
+
+	line, err := readInput(*input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	solveA(line)
+}
---
+++ 2021/16/src/main_test.go
@@ -0,0 +1,184 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"os"
+	"reflect"
+	"testing"
+
+	"github.com/simmonmt/aoc/2021/common/logger"
+)
+
+func TestHexToBin(t *testing.T) {
+	type TestCase struct {
+		in   string
+		want string
+	}
+
+	testCases := []TestCase{
+		TestCase{"D2FE28", "110100101111111000101000"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			if got, err := hexToBin(tc.in); err != nil || got != tc.want {
+				t.Errorf(`hexToBin("%v") = %v, %v, want %v, nil`,
+					tc.in, got, err, tc.want)
+			}
+		})
+	}
+}
+
+func TestArrView(t *testing.T) {
+	in := []byte{'a', 'b', 'c', 'd'}
+	v := NewArrView(in)
+
+	if got := v.Left(); got != 4 {
+		t.Fatalf("v.Left() = %v, want 4", got)
+	}
+	if got := v.Off(); got != 0 {
+		t.Fatalf("v.Left() = %v, want 0", got)
+	}
+
+	if got, err := v.Consume(2); err != nil || !reflect.DeepEqual(got, []byte{'a', 'b'}) {
+		t.Fatalf(`v.Consume(2) = %v, %v, want ['a','b'], nil`, got, err)
+	}
+
+	if got := v.Left(); got != 2 {
+		t.Fatalf("v.Left() = %v, want 2", got)
+	}
+	if got := v.Off(); got != 2 {
+		t.Fatalf("v.Left() = %v, want 2", got)
+	}
+
+	if got, err := v.Consume(1); err != nil || !reflect.DeepEqual(got, []byte{'c'}) {
+		t.Fatalf(`v.Consume(2) = %v, %v, want ['c'], nil`, got, err)
+	}
+
+	if got := v.Left(); got != 1 {
+		t.Fatalf("v.Left() = %v, want 1", got)
+	}
+	if got := v.Off(); got != 3 {
+		t.Fatalf("v.Left() = %v, want 3", got)
+	}
+
+	if got, err := v.Consume(9); err == nil {
+		t.Fatalf(`v.Consume(9) = %v, %v, want _, non-nil`, got, err)
+	}
+}
+
+func TestConsumeInt(t *testing.T) {
+	v := NewArrView([]byte("1101110"))
+	if got, err := consumeInt(v, 3); err != nil || got != 6 {
+		t.Fatalf(`consumeInt("11011", 3) = %v, %v; want 6, nil`, got, err)
+	}
+
+	if got := v.Left(); got != 4 {
+		t.Fatalf("v.Left() = %v, want 4", got)
+	}
+	if got := v.Off(); got != 3 {
+		t.Fatalf("v.Left() = %v, want 3", got)
+	}
+
+	if got, err := consumeInt(v, 3); err != nil || got != 7 {
+		t.Fatalf(`consumeInt("111", 3) = %v, %v; want 6, nil`, got, err)
+	}
+}
+
+func TestDecodeLiteral(t *testing.T) {
+	type TestCase struct {
+		in   string
+		want int
+	}
+
+	testCases := []TestCase{
+		TestCase{in: "00101", want: 5},
+		TestCase{in: "1010101100", want: 0x5c},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			in := NewArrView([]byte(tc.in))
+			if got, err := decodeLiteral(in); err != nil || got != tc.want {
+				t.Errorf(`decodeLiteral("%v") = %v, %v, want %v, nil`,
+					tc.in, got, err, tc.want)
+			}
+			if in.Left() != 0 {
+				t.Errorf("in un-empty: %+v", in)
+			}
+		})
+	}
+}
+
+func TestDecode(t *testing.T) {
+	type TestCase struct {
+		in         string
+		versionSum int
+	}
+
+	testCases := []TestCase{
+		TestCase{
+			in:         "D2FE28",
+			versionSum: 6,
+		},
+		TestCase{
+			in:         "38006F45291200",
+			versionSum: 9,
+		},
+		TestCase{
+			in:         "EE00D40C823060",
+			versionSum: 7 + 2 + 4 + 1,
+		},
+		TestCase{
+			in:         "8A004A801A8002F478",
+			versionSum: 4 + 1 + 5 + 6,
+		},
+		TestCase{
+			in:         "620080001611562C8802118E34",
+			versionSum: 12,
+		},
+		TestCase{
+			in:         "C0015000016115A2E0802F182340",
+			versionSum: 23,
+		},
+		TestCase{
+			in:         "A0016C880162017C3686B18A3D4780",
+			versionSum: 31,
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.in, func(t *testing.T) {
+			packet, err := decode(tc.in)
+			if err != nil {
+				t.Errorf(`decode("%v") = _, %v, want _, nil`,
+					tc.in, err)
+			}
+
+			packet.DumpTo(os.Stdout)
+
+			if got := versionSum(packet); got != tc.versionSum {
+				t.Errorf(`versionSum(_) = %v, want %v, nil`,
+					got, tc.versionSum)
+			}
+		})
+	}
+}
+
+func TestMain(m *testing.M) {
+	logger.Init(true)
+	os.Exit(m.Run())
+}
