05c9dd387b44e7a82199602223d314881f9ce2a1
Matthew Simmons <simmonmt@acm.org>
2023-12-03 18:28:26 -0500

    pylint for day 03 python

--- 2023/03/py/solution.py
+++ 2023/03/py/solution.py
@@ -1,14 +1,17 @@
-# Input is a 2x2 grid. Each cell is a '.' (empty), a digit, or a symbol.
-# Consecutive digits form numbers. Numbers are only horizontal. See the unit
-# test for sample input.
-#
-# Problem A: Find the sum of all numbers that are adjacent to a symbol (even
-#   diagonally).
-# Problem B: Some '*' characters have exactly two adjacent numbers. Return the
-#   sum of the product of the two numbers adjacent to each qualifying '*'.
-#
-# We make a point of processing the input as a stream. It's more efficient to
-# do it this way (vs reading the entire file into memory).
+"""The AOC 2023 day 03 solution code.
+
+Input is a 2x2 grid. Each cell is a '.' (empty), a digit, or a symbol.
+Consecutive digits form numbers. Numbers are only horizontal. See the unit
+test for sample input.
+
+Problem A: Find the sum of all numbers that are adjacent to a symbol (even
+  diagonally).
+Problem B: Some '*' characters have exactly two adjacent numbers. Return the
+  sum of the product of the two numbers adjacent to each qualifying '*'.
+
+We make a point of processing the input as a stream. It's more efficient to
+do it this way (vs reading the entire file into memory).
+"""
 
 from typing import Dict, Generator, Iterator, List, Tuple
 from collections import namedtuple
@@ -19,6 +22,13 @@ Pos = namedtuple('Pos', ['x', 'y'])
 
 
 def find_number(s: str, x: int) -> Tuple[int, int]:
+    """Find a number in a string.
+
+    Given a string like '...123...' we can be called with x set to any index
+    within the number (x=3..5). Returns the index of the first character of the
+    number as well as the number itself.
+    """
+
     start, end = x, x
     while start > 0 and s[start-1].isdigit():
         start -= 1
@@ -29,6 +39,14 @@ def find_number(s: str, x: int) -> Tuple[int, int]:
 
 
 def find_numbers_at(prev: str, cur: str, next_: str, x: int, y: int) -> List[Finding]:
+    """Find the numbers adjacent to a given character in the current line.
+
+    x is an index into cur. This function finds all numbers that have digits in
+    any of the cells adjacent to cur[x]. Adjacency includes diagonals. y gives the
+    y coordinate for cur, with prev and next_ containing the lines before and after,
+    respectively, cur.
+    """
+
     out: List[Finding] = []
 
     def save_number(s: str, x: int, y: int):
@@ -60,25 +78,43 @@ def find_numbers_at(prev: str, cur: str, next_: str, x: int, y: int) -> List[Fin
     return out
 
 
-def find_numbers(prev: str, cur: str, next: str, y: int) -> List[Finding]:
+def find_numbers(prev: str, cur: str, next_: str, y: int) -> List[Finding]:
+    """Find numbers adjacent to any symbol in the current line.
+
+    Returns any numbers (in the form of Finding instances) adjacent to each
+    symbols (non-digit, non-'.' character) in cur. y contains the y coordinate
+    for cur, used to calculate the location in returned findings.
+    """
+
     out: List[Finding] = []
 
     for x, c in enumerate(cur):
         if c == '.' or c.isdigit():
             continue  # We only want symbols
 
-        out.extend(find_numbers_at(prev, cur, next, x, y))
+        out.extend(find_numbers_at(prev, cur, next_, x, y))
 
     return out
 
 
-def walker(input: Iterator[str]) -> Generator[Lines, None, None]:
-    prev, next_ = "", next(input).rstrip()
+def walker(input_: Iterator[str]) -> Generator[Lines, None, None]:
+    """Walks an interator, returning previous, current, and next lines.
+
+    This generator is used by callers that process a stream, but need the
+    previous and next lines in addition to the current one. prev and next_
+    will be empty at the beginning and end of the iteration, respectively.
+    """
+
+    prev = ""
+    try:
+        next_ = next(input_).rstrip()
+    except StopIteration:
+        return
 
     while next_:
         cur = next_
         try:
-            next_ = next(input).rstrip()
+            next_ = next(input_).rstrip()
         except StopIteration:
             next_ = ""
 
@@ -87,18 +123,25 @@ def walker(input: Iterator[str]) -> Generator[Lines, None, None]:
         prev = cur
 
 
-def solve_a(input: Iterator[str]):
+def solve_a(input_: Iterator[str]):
+    """Solve part A."""
+
+    # A number could be adjacent to multiple symbols (maybe?). This dict
+    # ensures we process each number once.
     nums: Dict[Pos, int] = {}
-    for y, lines in enumerate(walker(input)):
+
+    for y, lines in enumerate(walker(input_)):
         for finding in find_numbers(lines.prev, lines.cur, lines.next_, y):
             nums[finding.loc] = finding.num
 
     return sum(nums.values())
 
 
-def solve_b(input: Iterator[str]):
+def solve_b(input_: Iterator[str]):
+    """Solve part B."""
+
     out = 0
-    for y, lines in enumerate(walker(input)):
+    for y, lines in enumerate(walker(input_)):
         for x, c in enumerate(lines.cur):
             if c != '*':
                 continue
