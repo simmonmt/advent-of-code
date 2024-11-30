791d0e7832c82b8ae519a8a94ff8bbe3507a3d8a
Matthew Simmons <simmonmt@acm.org>
2024-11-30 17:46:18 -0500

    Support multiple sample test data files

---
+++ 2024/common/testutils/samples.go
@@ -0,0 +1,91 @@
+package testutils
+
+import (
+	"bufio"
+	"iter"
+	"path"
+	"strconv"
+	"strings"
+
+	"github.com/simmonmt/aoc/2024/common/logger"
+)
+
+type SampleTestCase struct {
+	File         string
+	Body         []string
+	WantInput    []string
+	WantA, WantB int64
+}
+
+type SampleIter struct {
+	s *bufio.Scanner
+}
+
+func NewSampleIter(raw string) *SampleIter {
+	return &SampleIter{
+		s: bufio.NewScanner(strings.NewReader(raw)),
+	}
+}
+
+func (si *SampleIter) All() iter.Seq2[string, []string] {
+	nextLine := func() (string, bool) {
+		if !si.s.Scan() {
+			if err := si.s.Err(); err != nil {
+				panic("bad read")
+			}
+			return "", false
+		}
+
+		return si.s.Text(), true
+	}
+
+	return func(yield func(string, []string) bool) {
+		for {
+			fullPath, ok := nextLine()
+			if !ok {
+				return
+			}
+
+			numLines := -1
+			var err error
+			if nlStr, ok := nextLine(); !ok {
+				panic("missing num lines")
+			} else if numLines, err = strconv.Atoi(nlStr); err != nil {
+				panic("bad num lines")
+			}
+
+			out := make([]string, numLines)
+			for i := range numLines {
+				var ok bool
+				if out[i], ok = nextLine(); !ok {
+					panic("bad body")
+				}
+			}
+
+			if !yield(fullPath, out) {
+				return
+			}
+		}
+	}
+}
+
+func PopulateTestCases(raw string, testCases []SampleTestCase) {
+	i := 0
+	for fullPath, body := range NewSampleIter(raw).All() {
+		if i >= len(testCases) {
+			logger.Fatalf("too many samples; have %d cases",
+				len(testCases))
+		}
+
+		tc := &testCases[i]
+		tc.File = path.Base(fullPath)
+		tc.Body = body
+
+		i++
+	}
+
+	if i != len(testCases) {
+		logger.Fatalf("not enough samples; have %d cases",
+			len(testCases))
+	}
+}
--- 2024/tpl/src/main_test.go
+++ 2024/tpl/src/main_test.go
@@ -1,75 +1,77 @@
-// Copyright 2024 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package main
 
 import (
 	_ "embed"
 	"os"
-	"strings"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 	"github.com/simmonmt/aoc/2024/common/logger"
+	"github.com/simmonmt/aoc/2024/common/testutils"
 )
 
 var (
-	//go:embed sample.txt
-	rawSample   string
-	sampleLines []string
+	//go:embed combined_samples.txt
+	rawSample       string
+	sampleTestCases = []testutils.SampleTestCase{
+		testutils.SampleTestCase{
+			WantInput: []string{""},
+			WantA:     -1, WantB: -1,
+		},
+	}
 )
 
 func TestParseInput(t *testing.T) {
-	input, err := parseInput(sampleLines)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range sampleTestCases {
+		if tc.WantInput == nil {
+			continue
+		}
 
-	if diff := cmp.Diff(sampleLines, input); diff != "" {
-		t.Errorf("parseInput mismatch; -want,+got:\n%s\n", diff)
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tc.WantInput, input); diff != "" {
+				t.Errorf("parseInput mismatch; -want,+got:\n%s\n", diff)
+			}
+		})
 	}
 }
 
 func TestSolveA(t *testing.T) {
-	input, err := parseInput(sampleLines)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range sampleTestCases {
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if got, want := solveA(input), -1; got != want {
-		t.Errorf("solveA(sample) = %v, want %v", got, want)
+			if got := solveA(input); got != tc.WantA {
+				t.Errorf("solveA(sample) = %v, want %v", got, tc.WantA)
+			}
+		})
 	}
 }
 
 func TestSolveB(t *testing.T) {
-	input, err := parseInput(sampleLines)
-	if err != nil {
-		t.Fatal(err)
-	}
+	for _, tc := range sampleTestCases {
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if got, want := solveB(input), -1; got != want {
-		t.Errorf("solveB(sample) = %v, want %v", got, want)
+			if got := solveB(input); got != tc.WantB {
+				t.Errorf("solveB(sample) = %v, want %v", got, tc.WantB)
+			}
+		})
 	}
 }
 
 func TestMain(m *testing.M) {
 	logger.Init(true)
-
-	sampleLines = strings.Split(rawSample, "\n")
-	if len(sampleLines) > 0 && sampleLines[len(sampleLines)-1] == "" {
-		sampleLines = sampleLines[0 : len(sampleLines)-1]
-	}
-
+	testutils.PopulateTestCases(rawSample, sampleTestCases)
 	os.Exit(m.Run())
 }
