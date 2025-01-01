b6c65fa7487932b93caf019b957fd183aaa04297
Matthew Simmons <simmonmt@acm.org>
2025-01-01 15:07:39 -0500

    add SortedMapIter

--- 2024/common/collections/map.go
+++ 2024/common/collections/map.go
@@ -1,9 +1,28 @@
 package collections
 
+import (
+	"cmp"
+	"iter"
+	"slices"
+)
+
 func MapKeys[T comparable, U any](in map[T]U) []T {
 	out := []T{}
 	for k := range in {
 		out = append(out, k)
 	}
 	return out
+}
+
+func SortedMapIter[T cmp.Ordered, U any](in map[T]U) iter.Seq2[T, U] {
+	keys := MapKeys(in)
+	slices.Sort(keys)
+
+	return func(yield func(T, U) bool) {
+		for _, k := range keys {
+			if !yield(k, in[k]) {
+				return
+			}
+		}
+	}
 }
