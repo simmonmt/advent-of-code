2396df45f657c4903e77c710a26a9bb5064df539
Matthew Simmons <simmonmt@acm.org>
2025-01-03 14:29:24 -0500

    more map utils

--- 2024/common/collections/map.go
+++ 2024/common/collections/map.go
@@ -22,6 +22,24 @@ func MapValues[T comparable, U any](in map[T]U) []U {
 	return out
 }
 
+func CloneMap[T comparable, U any](in map[T]U) map[T]U {
+	out := make(map[T]U, len(in))
+	for k, v := range in {
+		out[k] = v
+	}
+	return out
+}
+
+func FilterMap[T comparable, U any](in map[T]U, filter func(key T, val U) bool) map[T]U {
+	out := map[T]U{}
+	for k, v := range in {
+		if filter(k, v) {
+			out[k] = v
+		}
+	}
+	return out
+}
+
 func OneMapKey[T comparable, U any](in map[T]U, def T) T {
 	for k := range in {
 		return k
