a301350d2695fada5357d6c6591166650aed5636
Matthew Simmons <simmonmt@acm.org>
2025-01-03 14:52:19 -0500

    fix 2024 day 24 test

--- 2024/24/src/main_test.go
+++ 2024/24/src/main_test.go
@@ -76,25 +76,6 @@ func TestSolveA(t *testing.T) {
 	}
 }
 
-func TestSolveB(t *testing.T) {
-	for _, tc := range sampleTestCases {
-		if tc.WantB == -1 {
-			continue
-		}
-
-		t.Run(tc.File, func(t *testing.T) {
-			input, err := parseInput(tc.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			if got := solveB(input); got != tc.WantB {
-				t.Errorf("solveB(sample) = %v, want %v", got, tc.WantB)
-			}
-		})
-	}
-}
-
 func TestMain(m *testing.M) {
 	logger.Init(true)
 	testutils.PopulateTestCases(rawSample, sampleTestCases)
