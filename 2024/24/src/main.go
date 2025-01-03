f046b8a5783b62bd0b8447a68be9f5cf4244fbee
Matthew Simmons <simmonmt@acm.org>
2025-01-03 14:29:34 -0500

    2024 day 24

---
+++ 2024/24/src/main.go
@@ -0,0 +1,385 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"regexp"
+	"runtime/pprof"
+	"sort"
+	"strings"
+
+	"github.com/simmonmt/aoc/2024/common/collections"
+	"github.com/simmonmt/aoc/2024/common/filereader"
+	"github.com/simmonmt/aoc/2024/common/logger"
+)
+
+var (
+	verbose      = flag.Bool("verbose", false, "verbose")
+	input        = flag.String("input", "", "input file")
+	cpuprofile   = flag.String("cpuprofile", "", "write cpu profile to file")
+	labelWires   = flag.Bool("label_wires", false, "label wires")
+	minimizeOuts = flag.String("minimize_outs", "", "comma-separated list of outs")
+
+	gatePattern = regexp.MustCompile(`^([^ ]+) (AND|XOR|OR) ([^ ]+) -> ([^ ]+)$`)
+)
+
+type State int
+type GateType int
+
+const (
+	ST_UNK State = iota
+	ST_OFF
+	ST_ON
+
+	GT_OR GateType = iota
+	GT_AND
+	GT_XOR
+)
+
+func (s State) String() string {
+	switch s {
+	case ST_UNK:
+		return "?"
+	case ST_OFF:
+		return "0"
+	case ST_ON:
+		return "1"
+	default:
+		panic("bad state")
+	}
+}
+
+func (gt GateType) String() string {
+	switch gt {
+	case GT_OR:
+		return "OR"
+	case GT_AND:
+		return "AND"
+	case GT_XOR:
+		return "XOR"
+	default:
+		panic("bad gate")
+	}
+}
+
+type Gate struct {
+	ID       int
+	In1, In2 string
+	Out      string
+	Type     GateType
+}
+
+type Input struct {
+	Wires map[string]State
+	Gates []Gate
+}
+
+func parseWires(lines []string) (map[string]State, error) {
+	out := map[string]State{}
+	for i, line := range lines {
+		name, val, ok := strings.Cut(line, ": ")
+		if !ok {
+			return nil, fmt.Errorf("wire %d: bad cut", i+1)
+		}
+
+		if val == "1" {
+			out[name] = ST_ON
+		} else {
+			out[name] = ST_OFF
+		}
+	}
+	return out, nil
+}
+
+func parseGates(lines []string) ([]Gate, error) {
+	gates := make([]Gate, len(lines))
+	for i, line := range lines {
+		parts := gatePattern.FindStringSubmatch(line)
+		if len(parts) != 5 {
+			return nil, fmt.Errorf("gate %d: bad match", i+1)
+		}
+
+		var gateType GateType
+		if t := parts[2]; t == "AND" {
+			gateType = GT_AND
+		} else if t == "OR" {
+			gateType = GT_OR
+		} else if t == "XOR" {
+			gateType = GT_XOR
+		} else {
+			panic("bad gate type")
+		}
+
+		gate := &gates[i]
+		gate.ID = i
+		gate.In1 = parts[1]
+		gate.In2 = parts[3]
+		gate.Out = parts[4]
+		gate.Type = gateType
+	}
+	return gates, nil
+}
+
+func parseInput(lines []string) (*Input, error) {
+	groups := filereader.BlankSeparatedGroupsFromLines(lines)
+	if len(groups) != 2 {
+		return nil, fmt.Errorf("bad groups, want 2, got %d", len(groups))
+	}
+
+	wires, err := parseWires(groups[0])
+	if err != nil {
+		return nil, fmt.Errorf("bad wires: %v", err)
+	}
+
+	gates, err := parseGates(groups[1])
+	if err != nil {
+		return nil, fmt.Errorf("bad gates: %v", err)
+	}
+
+	return &Input{
+		Wires: wires,
+		Gates: gates,
+	}, nil
+}
+
+func executeGate(gate *Gate, in1, in2 State) State {
+	toState := func(in bool) State {
+		if in {
+			return ST_ON
+		}
+		return ST_OFF
+	}
+
+	if in1 == ST_UNK || in2 == ST_UNK {
+		return ST_UNK
+	}
+
+	switch gate.Type {
+	case GT_OR:
+		return toState(in1 == ST_ON || in2 == ST_ON)
+	case GT_AND:
+		return toState(in1 == ST_ON && in2 == ST_ON)
+	case GT_XOR:
+		return toState(in1 != in2)
+	default:
+		panic("bad type")
+	}
+}
+
+func calculateSolution(wires map[string]State) int64 {
+	zWires := collections.FilterMap(wires, func(name string, _ State) bool {
+		return name[0] == 'z'
+	})
+
+	zNames := collections.MapKeys(zWires)
+	sort.Strings(zNames)
+
+	var out int64
+	for i := len(zNames) - 1; i >= 0; i-- {
+		var bit int64
+		if zWires[zNames[i]] == ST_ON {
+			bit = 1
+		}
+
+		out = (out << 1) | bit
+	}
+	return out
+}
+
+func solve(wiresIn map[string]State, gates []Gate) int64 {
+	solvedGates := map[*Gate]bool{}
+	wires := collections.CloneMap(wiresIn)
+
+	for len(solvedGates) != len(gates) {
+		for i := range len(gates) {
+			gate := &gates[i]
+			if solvedGates[gate] {
+				continue
+			}
+
+			out := executeGate(gate, wires[gate.In1], wires[gate.In2])
+			if out == ST_UNK {
+				continue
+			}
+
+			wires[gate.Out] = out
+			solvedGates[gate] = true
+		}
+
+		logger.Infof("solved gates: %d of %d", len(solvedGates), len(gates))
+	}
+	return calculateSolution(wires)
+}
+
+func solveA(input *Input) int64 {
+	return solve(input.Wires, input.Gates)
+}
+
+func makeDot(gates []Gate, input *Input) {
+	fmt.Println("digraph G {")
+
+	needInWires := map[string]bool{}
+	for _, gate := range gates {
+		fmt.Printf("gate%d_ [label=\"%s %d\"];\n", gate.ID, gate.Type, gate.ID)
+
+		for _, g2 := range gates {
+			if gate.Out == g2.In1 || gate.Out == g2.In2 {
+				label := ""
+				if *labelWires {
+					label = fmt.Sprintf("[label=\"%s\"]", gate.Out)
+				}
+
+				fmt.Printf("gate%d_ -> gate%d_ %s\n", gate.ID, g2.ID, label)
+			}
+		}
+
+		if in := gate.In1[0]; in == 'x' || in == 'y' || in == 'z' {
+			needInWires[gate.In1] = true
+			fmt.Printf("in_%s -> gate%d_\n", gate.In1, gate.ID)
+		}
+		if in := gate.In2[0]; in == 'x' || in == 'y' || in == 'z' {
+			needInWires[gate.In2] = true
+			fmt.Printf("in_%s -> gate%d_\n", gate.In2, gate.ID)
+		}
+		if out := gate.Out[0]; out == 'x' || out == 'y' || out == 'z' {
+			fmt.Printf("gate%d_ -> out_%s\n", gate.ID, gate.Out)
+		}
+	}
+
+	for wire := range needInWires {
+		state := input.Wires[wire]
+
+		var name string
+		if wire[0] == 'x' || wire[0] == 'y' {
+			name = fmt.Sprintf("in_%s", wire)
+		} else if wire[0] == 'z' {
+			name = fmt.Sprintf("out_%s", wire)
+		} else {
+			continue
+		}
+		label := fmt.Sprintf("%s = %s", name, state)
+
+		fmt.Printf("%s [label=\"%s\"]\n", name, label)
+	}
+
+	fmt.Println("}")
+}
+
+func minimize(gates []Gate, outs []string) []Gate {
+	needWires := map[string]bool{}
+	needGates := map[int]bool{}
+	for _, out := range outs {
+		needWires[out] = true
+	}
+
+	changed := true
+	for changed {
+		changed = false
+		for _, gate := range gates {
+			if !needWires[gate.Out] {
+				continue // doesn't help us
+			}
+			if needGates[gate.ID] {
+				continue // already processed
+			}
+
+			needGates[gate.ID] = true
+			needWires[gate.In1] = true
+			needWires[gate.In2] = true
+			changed = true
+		}
+	}
+
+	out := []Gate{}
+	for _, gate := range gates {
+		if _, found := needGates[gate.ID]; found {
+			out = append(out, gate)
+		}
+	}
+
+	return out
+}
+
+func findGate(gates []Gate, id int) *Gate {
+	for i := range gates {
+		if gates[i].ID == id {
+			return &gates[i]
+		}
+	}
+
+	panic(fmt.Sprintf("failed to find gate %d", id))
+}
+
+func swap(gates []Gate, a, b int) ([]Gate, []string) {
+	out := make([]Gate, len(gates))
+	copy(out, gates)
+
+	aGate := findGate(out, a)
+	bGate := findGate(out, b)
+
+	aGate.Out, bGate.Out = bGate.Out, aGate.Out
+	return out, []string{aGate.Out, bGate.Out}
+}
+
+func solveB(input *Input) string {
+	swaps := [][2]int{[2]int{83, 50}, [2]int{45, 81}, [2]int{23, 46}, [2]int{183, 63}}
+
+	gates := input.Gates
+
+	swappedWires := []string{}
+	for _, s := range swaps {
+		var wires []string
+		gates, wires = swap(gates, s[0], s[1])
+		swappedWires = append(swappedWires, wires...)
+	}
+
+	if *minimizeOuts != "" {
+		gates = minimize(gates, strings.Split(*minimizeOuts, ","))
+	}
+
+	// wires := map[string]State{}
+	// for i := 0; i < 45; i++ {
+	// 	wires[fmt.Sprintf("x%02d", i)] = ST_ON
+	// 	wires[fmt.Sprintf("y%02d", i)] = ST_OFF
+	// }
+
+	// fmt.Printf("%x\n", solve(wires, gates))
+
+	//makeDot(gates, input)
+
+	sort.Strings(swappedWires)
+	return strings.Join(swappedWires, ",")
+}
+
+func main() {
+	flag.Parse()
+	logger.Init(*verbose)
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
+
+	if *input == "" {
+		logger.Fatalf("--input is required")
+	}
+
+	lines, err := filereader.Lines(*input)
+	if err != nil {
+		logger.Fatalf("failed to read input: %v", err)
+	}
+
+	input, err := parseInput(lines)
+	if err != nil {
+		logger.Fatalf("failed to parse input: %v", err)
+	}
+
+	fmt.Println("A", solveA(input))
+	fmt.Println("B", solveB(input))
+}
---
+++ 2024/24/src/main_test.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	_ "embed"
+	"os"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/simmonmt/aoc/2024/common/logger"
+	"github.com/simmonmt/aoc/2024/common/testutils"
+)
+
+var (
+	//go:embed combined_samples.txt
+	rawSample       string
+	sampleTestCases = []testutils.SampleTestCase{
+		testutils.SampleTestCase{
+			WantInput: &Input{
+				Wires: map[string]State{
+					"x00": ST_ON,
+					"x01": ST_ON,
+					"x02": ST_ON,
+					"y00": ST_OFF,
+					"y01": ST_ON,
+					"y02": ST_OFF,
+				},
+				Gates: []Gate{
+					Gate{ID: 0, In1: "x00", In2: "y00", Out: "z00", Type: GT_AND},
+					Gate{ID: 1, In1: "x01", In2: "y01", Out: "z01", Type: GT_XOR},
+					Gate{ID: 2, In1: "x02", In2: "y02", Out: "z02", Type: GT_OR},
+				},
+			},
+			WantA: 4, WantB: -1,
+		},
+		testutils.SampleTestCase{
+			WantA: 2024, WantB: -1,
+		},
+	}
+)
+
+func TestParseInput(t *testing.T) {
+	for _, tc := range sampleTestCases {
+		if tc.WantInput == nil {
+			continue
+		}
+
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if diff := cmp.Diff(tc.WantInput, input); diff != "" {
+				t.Errorf("parseInput mismatch; -want,+got:\n%s\n", diff)
+			}
+		})
+	}
+}
+
+func TestSolveA(t *testing.T) {
+	for _, tc := range sampleTestCases {
+		if tc.WantA == -1 {
+			continue
+		}
+
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got := solveA(input); got != tc.WantA {
+				t.Errorf("solveA(sample) = %v, want %v", got, tc.WantA)
+			}
+		})
+	}
+}
+
+func TestSolveB(t *testing.T) {
+	for _, tc := range sampleTestCases {
+		if tc.WantB == -1 {
+			continue
+		}
+
+		t.Run(tc.File, func(t *testing.T) {
+			input, err := parseInput(tc.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if got := solveB(input); got != tc.WantB {
+				t.Errorf("solveB(sample) = %v, want %v", got, tc.WantB)
+			}
+		})
+	}
+}
+
+func TestMain(m *testing.M) {
+	logger.Init(true)
+	testutils.PopulateTestCases(rawSample, sampleTestCases)
+	os.Exit(m.Run())
+}
